b7be580938a87fcc94113c38e38cbae1625b016a
StarCloudDream <296920362@qq.com>
2025-06-18 09:13:08 +0800

    Update FoggPlugin.java
    删除怪物清除功能

--- src/main/java/cn/mcscd/fogg/FoggPlugin.java
+++ src/main/java/cn/mcscd/fogg/FoggPlugin.java
@@ -2,7 +2,6 @@ package cn.mcscd.fogg;
 
 import cn.mcscd.fogg.afk.AFKManager;
 import cn.mcscd.fogg.listener.ScissorsListener;
-import cn.mcscd.fogg.listener.EntityOptimizationListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FoggPlugin extends JavaPlugin {
@@ -20,8 +19,6 @@ public class FoggPlugin extends JavaPlugin {
 
  
         getServer().getPluginManager().registerEvents(new ScissorsListener(this), this);
- 
-        getServer().getPluginManager().registerEvents(new EntityOptimizationListener(this), this);
 
         getLogger().info("Fogg插件已启用，AFK和剪刀功能已激活");
     }
